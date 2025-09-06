f153cff133dec7a5b174b239b0a090def165d8ef
ethanmtamu <157394712+ethanmtamu@users.noreply.github.com>
2025-09-05 23:34:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuTab = document.querySelector('.menu-tab');
     const menu = document.querySelector('.menu');
+    const tempMenu = document.querySelector('.temp-menu')
 
     if(!menuTab || !menu) {
         console.error("Cannot find .menu-tab or .menu element");
@@ -10,5 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     menuTab.addEventListener('click', () => {
         menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+        tempMenu.style.display = tempMenu.style.display === 'none' ? 'block' : 'none';
     });
 });
