bcc111152abd9be2ffcbb31b23b7d64191189aa4
ethanmtamu <157394712+ethanmtamu@users.noreply.github.com>
2025-09-17 23:58:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuTab = document.querySelector('.menu-tab');
     const tabHandle = document.querySelector('.tab-handle')
     const menuLabel = document.querySelector('.menu-label');
+    const bodies = document.querySelectorAll('.tch-body');
+    const lb1 = document.querySelectorAll('.lb1inner');
+    const lb2 = document.querySelectorAll('.lb2inner');
+    const lb3 = document.querySelectorAll('.lb3inner');
+    let current = 0;
+    let isActive = false;
 
     tabHandle.addEventListener("click", () => {
         menuTab.classList.toggle("expanded");
@@ -15,4 +21,75 @@ document.addEventListener("DOMContentLoaded", () => {
             menuLabel.textContent ="menu?";
         }
     });
+
+    document.getElementById('upbtn').addEventListener('click', () => {
+        if (isActive) return;
+        isActive = true;
+
+        let old = current;
+        current = (current + 1) % bodies.length;
+        
+        bodies[old].classList.remove('active');
+        bodies[old].classList.add('exit');
+        if(lb1.length !== 0) {
+            lb1[old].classList.remove('active');
+            lb1[old].classList.add('exit');
+            lb2[old].classList.remove('active');
+            lb2[old].classList.add('exit');
+            lb3[old].classList.remove('active');
+            lb3[old].classList.add('exit');
+        }
+
+        setTimeout(() =>{
+            void bodies[current].offsetWidth;
+            bodies[current].classList.add('active');
+            if(lb1.length !== 0) {
+                void lb1[current].offsetWidth;
+                lb1[current].classList.add('active');
+                void lb2[current].offsetWidth;
+                lb2[current].classList.add('active');
+                void lb3[current].offsetWidth;
+                lb3[current].classList.add('active');
+            }
+        }, 450);
+
+        bodies[old].addEventListener('transitionend', function handler() {
+            bodies[old].classList.remove('exit');
+            if(lb1.length !== 0) {
+                lb1[old].classList.remove('exit');
+                lb2[old].classList.remove('exit');
+                lb3[old].classList.remove('exit');
+            }
+            bodies[old].removeEventListener('transitionend', handler);
+            isActive = false;
+        });
+    });
+
+    document.getElementById('dwnbtn').addEventListener('click', () => {
+        if (isActive) return;
+        isActive = true;
+
+        let old = current;
+        if(current == 0) {
+            current = bodies.length - 1;
+        }
+        else {
+            current = current - 1;
+        }
+        
+        bodies[old].classList.remove('active');
+        bodies[old].classList.add('exit');
+
+        setTimeout(() =>{
+            void bodies[current].offsetWidth;
+            bodies[current].classList.add('active');
+        }, 450);
+
+        bodies[old].addEventListener('transitionend', function handler() {
+            bodies[old].classList.remove('exit');
+            bodies[old].removeEventListener('transitionend', handler);
+            isActive = false;
+        });
+    });
 });
+
